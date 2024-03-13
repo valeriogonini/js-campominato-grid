e3bd781b28cebbea7dfc059bfeb43b6abd2b03ae
valeriogonini <valeriogonini@gmail.com>
2024-03-13 14:58:28 +0100

    add js

---
+++ js/app.js
@@ -0,0 +1,32 @@
+console.log("ciao");
+
+// L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+const ButtonPlay = document.querySelector(".play")
+const CellGrid = document.querySelector(".grid-cell")
+console.log(CellGrid, ButtonPlay);
+
+
+const size = 10
+const NumCell = size * size
+ButtonPlay.addEventListener("click", function () {
+    for (let i = 0; i < NumCell; i++) {
+        const num = i + 1;
+
+        const cellElement = document.createElement("div")
+        cellElement.className = "cell"
+        cellElement.innerHTML = num
+
+        CellGrid.append(cellElement)
+
+        cellElement.addEventListener("click", function () {
+            console.log(num);
+
+            cellElement.classList.toggle("bg-blue")
+
+        })
+    }
+})
