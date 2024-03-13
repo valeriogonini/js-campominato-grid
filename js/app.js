d7348686d64fc5299c57df263237cee611689de4
valeriogonini <valeriogonini@gmail.com>
2024-03-13 16:50:18 +0100

    add bonus

--- js/app.js
+++ js/app.js
@@ -7,11 +7,37 @@ console.log(CellGrid, ButtonPlay);
 
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 
-const size = 10
-const NumCell = size * size
+
 // L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
 
+
+
+
+
+
 ButtonPlay.addEventListener("click", function () {
+
+    const Select = document.getElementById("difficolta")
+
+    let size;
+
+
+    if (Select.value === "difficolta1") {
+        size = 10
+        CellGrid.classList.add("easy")
+
+    } else if (Select.value === "difficolta2") {
+        size = 9
+        CellGrid.classList.add("medium")
+
+    } else {
+        size = 7
+        CellGrid.classList.add("hard")
+
+    }
+
+    const NumCell = size * size
+
     for (let i = 0; i < NumCell; i++) {
         // Ogni cella ha un numero progressivo, da 1 a 100.
 
@@ -37,3 +63,5 @@ ButtonPlay.addEventListener("click", function () {
         })
     }
 })
+
+
