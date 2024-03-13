46864075d80372d8cc2bd9ce66cc722bda86598a
valeriogonini <valeriogonini@gmail.com>
2024-03-13 15:14:27 +0100

    add footer

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 console.log("ciao");
 
-// L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
-// Ogni cella ha un numero progressivo, da 1 a 100.
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
 const ButtonPlay = document.querySelector(".play")
 const CellGrid = document.querySelector(".grid-cell")
 console.log(CellGrid, ButtonPlay);
 
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 
 const size = 10
 const NumCell = size * size
+// L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+
 ButtonPlay.addEventListener("click", function () {
     for (let i = 0; i < NumCell; i++) {
+        // Ogni cella ha un numero progressivo, da 1 a 100.
+
         const num = i + 1;
 
         const cellElement = document.createElement("div")
@@ -21,6 +22,8 @@ ButtonPlay.addEventListener("click", function () {
         cellElement.innerHTML = num
 
         CellGrid.append(cellElement)
+        // // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro 
+        // ed emetto un messaggio in console con il numero della cella cliccata.
 
         cellElement.addEventListener("click", function () {
             console.log(num);
